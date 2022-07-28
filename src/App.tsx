a768f8e1dc129fbfc6e628a372f6039ce2722932
Panduss <zsuzsi.hudacsko@gmail.com>
2022-07-28 19:06:29 +0200

    need to add more basic setup stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.scss';
 import Welcome from "./pages/Welcome";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
+        <div className={styles.app}>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Welcome/>}/>
