a61b5d1b73b34b76b5603469ee18aad8b1f6d6c3
Panduss <zsuzsi.hudacsko@gmail.com>
2022-07-29 07:50:49 +0200

    removed typings

--- src/global.d.ts
+++
@@ -1,3 +0,0 @@
-export type WindowLocation<S = LocationState> = Window['location'] & HLocation<S>;
-export function useLocation(): WindowLocation;
-export function useLocation<T = {}>(): WindowLocation<T>;
